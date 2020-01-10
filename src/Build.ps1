ad68d9cf6480db4e8e6f96456433b836ee8a30ec
Thierry Lelegard <thierry@lelegard.fr>
2020-01-10 10:44:34 +0100

    Added sample C++ code

---
+++ src/Build.ps1
@@ -0,0 +1,16 @@
+﻿param([string]$Arch = "x64")
+
+$MSRoots = @("C:\Program Files*\MSBuild", "C:\Program Files*\Microsoft Visual Studio")
+
+$MSBuild = Get-ChildItem -Recurse -Path $MSRoots -Include MSBuild.exe -ErrorAction Ignore |
+    ForEach-Object { (Get-Command $_).FileVersionInfo } |
+    Sort-Object -Unique -Property FileVersion |
+    ForEach-Object { $_.FileName} |
+    Select-Object -Last 1
+
+if (-not $MSBuild) {
+    Write-Host "ERROR: MSBuild not found"
+    exit 1
+}
+
+& $MSBuild "$PSScriptRoot\test.sln" /nologo /maxcpucount /property:Configuration=Release /property:Platform=$Arch
