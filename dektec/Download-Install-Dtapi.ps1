bbd977aed5f7309a4bdee2ca0eb86483c04116a4
Thierry Lelegard <thierry@lelegard.fr>
2020-01-10 23:31:36 +0100

    test

--- dektec/Download-Install-Dtapi.ps1
+++ dektec/Download-Install-Dtapi.ps1
@@ -80,7 +80,6 @@ function Exit-Script([string]$Message = "")
 $ProgressPreference = 'SilentlyContinue'
 
 # Get the HTML page for Dektec SDK downloads.
-Write-Output "Reading ${DektecUrl}"
 $status = 0
 $message = ""
 try {
